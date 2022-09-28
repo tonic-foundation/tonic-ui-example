6849548342a8ef7f41e689664a5ad3bca4e8394f
renthog <58467174+renthog@users.noreply.github.com>
2022-09-28 14:30:06 -0700

    wallet connector: use NEAR_ENV

--- state/WalletSelectorContainer.tsx
+++ state/WalletSelectorContainer.tsx
@@ -15,7 +15,7 @@ import { setupSender } from '@near-wallet-selector/sender';
 import { setupNightly } from '@near-wallet-selector/nightly';
 import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
 
-import { IS_DEV } from '~/config';
+import { IS_DEV, NEAR_ENV } from '~/config';
 import { near } from '~/services/near';
 
 declare global {
@@ -42,7 +42,7 @@ export const WalletSelectorContextProvider: React.FC = ({ children }) => {
 
   const init = useCallback(async () => {
     const _selector = await setupWalletSelector({
-      network: 'mainnet',
+      network: NEAR_ENV,
       debug: IS_DEV,
       modules: [
         setupNearWallet(),
