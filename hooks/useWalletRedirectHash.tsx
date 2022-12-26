f4f0c7dfb9831133f677f114cedac496ccfad550
renthog <58467174+renthog@users.noreply.github.com>
2022-12-26 13:38:05 -0600

    use new transaction status library functions

--- hooks/useWalletRedirectHash.tsx
+++ hooks/useWalletRedirectHash.tsx
@@ -1,15 +1,10 @@
-// TODO: good candidate for public repo
-import {
-  FinalExecutionOutcome,
-  JsonRpcProvider,
-} from 'near-api-js/lib/providers';
-import { ExecutionOutcome } from 'near-api-js/lib/providers/provider';
 import React, { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import { useSearchParam } from 'react-use';
 import CannedToast from '~/components/common/CannedToast';
 import { wrappedToast } from '~/components/common/ToastWrapper';
 import { near } from '~/services/near';
+import { didTxSucceed } from '@tonic-foundation/transaction/lib/status';
 import { useWalletSelector } from '~/state/WalletSelectorContainer';
 
 /**
@@ -23,38 +18,9 @@ export default function useWalletRedirectHash() {
   return { txId };
 }
 
-function isErrorStatus(
-  s: ExecutionOutcome['status'] | FinalExecutionOutcome['status']
-): boolean {
-  if (typeof s === 'string') {
-    return s === 'Failure';
-  }
-  return 'Failure' in s;
-}
-
-/**
- *
- * @param txId
- * @returns true if success, false otherwise
- */
-async function didTxSucceed(accountId: string, txId: string): Promise<boolean> {
-  // It's not exactly this. Actual rpc response has more fields, but we only
-  // need these fields for what we're doing. RPC will throw its own errors,
-  // so it's fine to use the type as is.
-  const ret: FinalExecutionOutcome = await (
-    near.connection.provider as JsonRpcProvider
-  ).sendJsonRpc('EXPERIMENTAL_tx_status', [txId, accountId]);
-
-  const hasError = ret.receipts_outcome.some((o) => {
-    return isErrorStatus(o.outcome.status);
-  });
-
-  return !isErrorStatus(ret.status) && !hasError;
-}
-
 async function checkAndToastTx(accountId: string, id: string) {
   try {
-    const succeeded = await didTxSucceed(accountId, id);
+    const succeeded = await didTxSucceed(near, accountId, id);
     toast.custom(
       wrappedToast(<CannedToast.TxGeneric id={id} succeeded={succeeded} />, {
         variant: succeeded ? 'success' : 'error',
